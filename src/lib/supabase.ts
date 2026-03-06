9d4fee52c14d595035c55494140fedb09bf10747
yoni marco <yoni.marco2002@gmail.com>
2026-03-06 21:50:13 +0200

    fix: fallback to hardcoded supabase keys to prevent fatal crash on web if expoConfig is undefined

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
 
-const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl as string;
-const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey as string;
+const supabaseUrl = (Constants.expoConfig?.extra?.supabaseUrl || 'https://anhfanmdotbzbpmimeem.supabase.co') as string;
+const supabaseAnonKey = (Constants.expoConfig?.extra?.supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuaGZhbm1kb3RiemJwbWltZWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MzU3ODksImV4cCI6MjA4ODMxMTc4OX0._mbaSi5F3zDZREyvvlLQ4ZxOzU5KiaXXiQz_b1_FD4A') as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
